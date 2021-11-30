a7bd1dfc672320793c82bcd620897fba48113c1f
Spektra <bergman.william@elev.ga.ntig.se>
2021-11-30 13:08:50 +0100

    test

---
+++ program_1/program.cs
@@ -0,0 +1,13 @@
+using System;
+
+Console.WriteLine("Which gender are you?");
+var name = Console.ReadLine();
+
+if (name == "Male")
+{
+    Console.WriteLine("Access Granted")
+}
+else ();
+{
+    Console.WriteLine("Access Denied")
+}
